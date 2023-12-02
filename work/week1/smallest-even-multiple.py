03ea21892e6d216ccbd56df7f256038c089364ba
Afomiat <106989621+Afomiat@users.noreply.github.com>
2023-12-02 00:29:47 -0500

    Added solution for Smallest Even Multiple

---
+++ work/week1/smallest-even-multiple.py
@@ -0,0 +1,22 @@
+class Solution:
+    def smallestEvenMultiple(self, n: int) -> int:
+        return (n*2) if n%2 != 0 else n
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        # if n % 2!= 0:
+        #     return n*2
+        # else:
+        #     return n
