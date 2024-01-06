634b043cd295b0cb2fcfad2a3edabe422c13a7ca
Afomiat <106989621+Afomiat@users.noreply.github.com>
2024-01-06 07:36:11 -0500

    Added solution for Sort Colors

--- work/week1/sort-colors.py
+++ work/week1/sort-colors.py
@@ -1,21 +1,29 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
+        
+        l = 0 
+        mid = 0 
+        r = len(nums) - 1  
+
+        while mid <= r:
+            if nums[mid] == 0:
+                
+                nums[mid], nums[l] = nums[l], nums[mid]
+                l += 1
+                mid += 1
+            elif nums[mid] == 1:
+               
+                mid += 1
+            else:
+                nums[mid], nums[r] = nums[r], nums[mid]
+                r -= 1
+
+
+
+
+
+
         """
         Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)
-        for i in range(n):
-            for j in range(0,n - i -1):
-                if nums[j] > nums[j + 1]:
-                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
-        # result = []
-        # my_dict = Counter(nums)
-       
-        # for j in range(len(my_dict)):
-        #     min_key = min(my_dict, key = int)
-        #     for i in range(my_dict[min_key]):
-        #         result.append(min_key)
-        #     del my_dict[min_key]
-        # nums = result
-            
-       
+        
