162b5aaf92498bb1fbf9585f11c182a1cc1bb578
Afomiat <106989621+Afomiat@users.noreply.github.com>
2024-03-04 21:18:57 -0500

    Add solution for Combinations

---
+++ leetcode/combinations.py
@@ -0,0 +1,14 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+        def helper( first, path):
+            if len(path) == k:
+                res.append(path[:])
+                return
+            for i in range(first, n + 1):
+                path.append(i)
+                helper(i + 1, path)
+                path.pop()
+        
+        helper(1, []) 
+        return res           
